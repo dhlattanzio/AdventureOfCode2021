9fa13c3da230ed719596df292acef20918cd5072
Diego Lattanzio <lattanzio.diego@gmail.com>
2021-12-06 05:26:32 -0300

    day 6

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace day6
 {
@@ -7,9 +8,65 @@ namespace day6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-- Day 4 --");
+            new Program();
+        }
+        
+        public Program()
+        {
+            Console.WriteLine("-- Day 6 --");
             string input = File.ReadAllText("input/input.txt").Trim();
-            string[] lines = input.Split("\r\n\r\n");
+            int[] startFish = input.Split(",").Select(int.Parse).ToArray();
+            
+            // Start
+            long[] fish = new long[9];
+            foreach (var f in startFish)
+            {
+                fish[f]++;
+            }
+
+            long[] fishCopy = new long[9];
+            fish.CopyTo(fishCopy, 0);
+
+            // 80 Days
+            for (int i = 0; i < 80; i++)
+            {
+                fish = Tick(fish);
+            }
+            
+            Console.WriteLine("Result part 1: " + GetTotalFish(fish));
+
+            fish = fishCopy;
+            // 256 Days
+            for (int i = 0; i < 256; i++)
+            {
+                fish = Tick(fish);
+            }
+            
+            Console.WriteLine("Result part 2: " + GetTotalFish(fish));
+        }
+
+        public long GetTotalFish(long[] fish)
+        {
+            return fish.Aggregate<long, long>(0, (acc, f) => acc + f);
+        }
+
+        public long[] Tick(long[] fish)
+        {
+            long[] newFish = new long[fish.Length];
+            for (int i = fish.Length - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    newFish[8] += fish[i];
+                    newFish[6] += fish[0];
+                }
+                else
+                {
+                    newFish[i - 1] += fish[i];
+                }
+            }
+
+            return newFish;
         }
     }
 }
